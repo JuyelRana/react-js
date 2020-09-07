83aea94b0aa8f424704e5445c970d832ab6cf52d
JuyelRana <mjuyelrana@gmail.com>
2020-09-08 00:19:59 +0600

    File Structure

--- src/App.js
+++ src/App.js
@@ -1,86 +1,26 @@
-import React, {Component} from 'react';
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Person from './Components/Person/Person';
 
-
-class App extends Component {
-    state = {
-        persons: [
-            {id: '1302055', name: 'Mijan', age: 25},
-            {id: '1302056', name: 'Saumen Mondal', age: 23},
-            {id: '1302057', name: 'Nazmul Hasan', age: 27},
-            {id: '1302058', name: 'Juyel Rana', age: 26}
-        ],
-        otherState: 'Somer Other Value',
-        showPersons: false
-    }
-
-    nameChangedHandler = (event, id) => {
-        const personIndex = this.state.persons.findIndex(p => {
-            return p.id === id;
-        });
-
-        const person = {
-            ...this.state.persons[personIndex]
-        };
-
-        person.name = event.target.value;
-
-        const persons = [...this.state.persons];
-        persons[personIndex] = person;
-        this.setState({persons: persons})
-    }
-
-    deletePersonHandler = (personIndex) => {
-        const persons = [...this.state.persons];
-        persons.splice(personIndex, 1);
-        this.setState({persons: persons});
-    }
-
-    togglePersonHandler = () => {
-        const doesShow = this.state.showPersons;
-        this.setState({showPersons: !doesShow})
-    }
-
-    render() {
-
-        let persons = null;
-
-        if (this.state.showPersons) {
-            persons = (
-                <div>
-                    {this.state.persons.map((person, index) => {
-                        return <Person
-                            click={() => this.deletePersonHandler(index)}
-                            name={person.name}
-                            age={person.age}
-                            key={person.id}
-                            changed={(event) => this.nameChangedHandler(event, person.id)}/>
-                    })}
-                </div>
-            )
-
-        }
-
-        const assignedClasses = [];
-
-        if (this.state.persons.length <= 2) {
-            assignedClasses.push('red');
-        }
-
-        if (this.state.persons.length <= 1) {
-            assignedClasses.push('bold');
-        }
-
-        return (<div className="App">
-            <h1>React Developer</h1>
-            <p className={assignedClasses.join(' ')}>Working with react</p>
-            <button onClick={this.togglePersonHandler} className="button">
-                Toggle Person
-            </button>
-            {persons}
-        </div>)
-    }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello World
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React Juyel Rana
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
